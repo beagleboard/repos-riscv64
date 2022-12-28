2e9ceaf6c05703b2541024c67c050882ff26356e
Robert Nelson <robertcnelson@gmail.com>
2022-12-28 14:18:29 -0600

    gt (0.0.0-git20210712.8)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-gt/version.sh
+++ git-gt/version.sh
@@ -4,9 +4,9 @@
 
 package_name="gt"
 debian_pkg_name="${package_name}"
-package_version="0.0.0-git20210712.6"
-package_source="${package_name}-${package_version}.orig.tar.xz"
-src_dir="${package_name}-${package_version}"
+package_version="0.0.0-git20210712.8"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/linux-usb-gadgets/gt"
 git_sha="31b135546d0d1733b5b7ca19f48748c06824d65a"
@@ -17,4 +17,11 @@ debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 
-sid_version="~sid+20210818"
+clear_changelog="enable"
+
+buster_version="~buster+20220713"
+bullseye_version="~bullseye+20220713"
+bionic_version="~bionic+20220713"
+focal_version="~focal+20220713"
+sid_version="~sid+20221228"
+lunar_version="~lunar+20221228"
