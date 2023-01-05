7ff2becabb94f1ab9534f90ea23c003ac3a5f302
Robert Nelson <robertcnelson@gmail.com>
2023-01-05 16:11:39 -0600

    wpa (2:2.10-10bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-12-wpa/version.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+#https://tracker.debian.org/pkg/wpa
+
+mirror="http://http.debian.net/debian"
+
+package_name="wpa"
+debian_pkg_name="${package_name}"
+epoch="2"
+package_version="2.10"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-2.10"
+
+git_repo=""
+git_sha=""
+reprepro_dir="w/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-10"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="bbbio0"
+
+sid_version="~sid+20230105"
+lunar_version="~lunar+20230105"
