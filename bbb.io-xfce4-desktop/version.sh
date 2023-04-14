78659d7a25b7e4e14619a83595dbbc9571ac0733
Robert Nelson <robertcnelson@gmail.com>
2023-04-14 15:26:26 -0500

    bbb.io-xfce4-desktop (1.20230413.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bbb.io-xfce4-desktop/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+package_name="bbb.io-xfce4-desktop"
+debian_pkg_name="${package_name}"
+package_version="1.20230413.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+sid_version="~sid+20230414"
+lunar_version="~lunar+20230414"
