388b1f87fb909cf77fb561b3e2733c8cbf655f61
Robert Nelson <robertcnelson@gmail.com>
2023-08-17 12:03:42 -0500

    libusbgx (0.2.0-git20230728.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-libusbgx/version.sh
+++ git-libusbgx/version.sh
@@ -4,12 +4,12 @@
 
 package_name="libusbgx"
 debian_pkg_name="${package_name}"
-package_version="0.2.0-git20210811.3"
+package_version="0.2.0-git20230728.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/linux-usb-gadgets/libusbgx"
-git_sha="fa4e7e92260e33f78b72e7796b3df28c4c84b8e2"
+git_sha="3cc5af771cb049bbaf3d0a0ebf5d843b932ec9f8"
 reprepro_dir="libu/${package_name}"
 dl_path=""
 
@@ -19,5 +19,6 @@ debian_diff=""
 
 clear_changelog="enable"
 
-sid_version="~sid+20221228"
-lunar_version="~lunar+20221228"
+sid_version="~sid+20230817"
+lunar_version="~lunar+20230817"
+mantic_version="~mantic+20230817"
