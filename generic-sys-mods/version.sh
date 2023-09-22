2bd1cfd0a3de3e27c95dafd2f7d78d6abaf78843
Robert Nelson <robertcnelson@gmail.com>
2023-09-22 09:52:24 -0500

    generic-sys-mods (1.20230922.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- generic-sys-mods/version.sh
+++ generic-sys-mods/version.sh
@@ -4,7 +4,7 @@
 
 package_name="generic-sys-mods"
 debian_pkg_name="${package_name}"
-package_version="1.20230921.1"
+package_version="1.20230922.0"
 package_source=""
 src_dir=""
 
@@ -19,6 +19,6 @@ debian_patch=""
 
 clear_changelog="enable"
 
-sid_version="~sid+20230921"
-lunar_version="~lunar+20230921"
-mantic_version="~mantic+20230921"
+sid_version="~sid+20230922"
+lunar_version="~lunar+20230922"
+mantic_version="~mantic+20230922"
