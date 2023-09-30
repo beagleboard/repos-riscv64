b20568c34f4052c22d2e1e242f5af24749496e67
Robert Nelson <robertcnelson@gmail.com>
2023-09-30 10:34:08 -0500

    bb-customizations (1.20230930.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20230929.0"
+package_version="1.20230930.0"
 package_source=""
 src_dir=""
 
@@ -19,6 +19,6 @@ debian_patch=""
 
 clear_changelog="enable"
 
-sid_version="~sid+20230929"
-lunar_version="~lunar+20230929"
-mantic_version="~mantic+20230929"
+sid_version="~sid+20230930"
+lunar_version="~lunar+20230930"
+mantic_version="~mantic+20230930"
