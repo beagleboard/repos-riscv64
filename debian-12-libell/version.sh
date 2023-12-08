de029b861024c3c56f5674676f428c3c35f3efbb
Robert Nelson <robertcnelson@gmail.com>
2023-12-08 11:01:12 -0600

    ell (0.61-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-libell/version.sh
+++ debian-12-libell/version.sh
@@ -7,7 +7,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="ell"
 debian_pkg_name="${package_name}"
-package_version="0.58"
+package_version="0.61"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -16,9 +16,10 @@ git_sha=""
 reprepro_dir="e/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="bbbio2"
+local_patch="bbbio0"
 
-lunar_version="~lunar+20230919"
+lunar_version="~lunar+20231208"
+mantic_version="~mantic+20231208"
