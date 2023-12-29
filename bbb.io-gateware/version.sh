2995f1fb83fc2249598609670bbdb93fd75c3a6c
Robert Nelson <robertcnelson@gmail.com>
2023-12-29 10:11:32 -0600

    bbb.io-gateware (1.20231229.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-gateware/version.sh
+++ bbb.io-gateware/version.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-VERSION="BVF-0.4.0-26-g0d225e4"
-DATEC="1.20231226.0"
+VERSION="BVF-0.4.0-27-g7078de9"
+DATEC="1.20231228.0"
 
 package_name="bbb.io-gateware"
 debian_pkg_name="${package_name}"
-package_version="1.20231227.2"
+package_version="1.20231229.0"
 package_source=""
 src_dir=""
 
@@ -22,7 +22,7 @@ debian_dl_1="https://git.beagleboard.org/beaglev-fire/gateware-snapshots/-/raw/m
 
 clear_changelog="enable"
 
-sid_version="~sid+20231227"
-lunar_version="~lunar+20231227"
-mantic_version="~mantic+20231227"
-noble_version="~noble+20231227"
+sid_version="~sid+20231229"
+lunar_version="~lunar+20231229"
+mantic_version="~mantic+20231229"
+noble_version="~noble+20231229"
