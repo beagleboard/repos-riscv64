b058d56eb3e35415264a95b0ca8a502688023b1f
Robert Nelson <robertcnelson@gmail.com>
2024-05-21 14:44:23 -0500

    bb-usb-gadgets (1.20240521.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-usb-gadgets/version.sh
+++ bb-usb-gadgets/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-usb-gadgets"
 debian_pkg_name="${package_name}"
-package_version="1.20240314.2"
+package_version="1.20240521.0"
 package_source=""
 src_dir=""
 
@@ -17,7 +17,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-sid_version="~sid+20240418"
-lunar_version="~lunar+20240418"
-mantic_version="~mantic+20240418"
-noble_version="~noble+20240418"
+sid_version="~sid+20240521"
+lunar_version="~lunar+20240521"
+mantic_version="~mantic+20240521"
+noble_version="~noble+20240521"
