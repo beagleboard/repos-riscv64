219851507859ba9fed39ca63b24addca9d274d80
Robert Nelson <robertcnelson@gmail.com>
2024-06-06 17:34:09 -0500

    iwd (2.18-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-iwd/version.sh
+++ debian-12-iwd/version.sh
@@ -6,7 +6,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="iwd"
 debian_pkg_name="${package_name}"
-package_version="2.17"
+package_version="2.18"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -21,5 +21,6 @@ debian_patch=""
 local_patch="bbbio0"
 
 sid_version="~sid+20240115"
-lunar_version="~lunar+20240402"
-mantic_version="~mantic+20240402"
+lunar_version="~lunar+20240606"
+mantic_version="~mantic+20240606"
+noble_version="~noble+20240606"
