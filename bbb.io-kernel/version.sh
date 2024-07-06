c1d29bac24d134d671302203b34c9bde40daab7e
Robert Nelson <robertcnelson@gmail.com>
2024-07-06 13:10:54 -0500

    bbb.io-kernel (1.20240706.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bbb.io-kernel/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+package_name="bbb.io-kernel"
+debian_pkg_name="${package_name}"
+package_version="1.20240706.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+lunar_version="~lunar+20240706"
+mantic_version="~mantic+20240706"
+noble_version="~noble+20240706"
