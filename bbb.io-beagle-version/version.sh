491b9e2bc80927bfbe9edaf29f5f52ca8eb3bcc5
Robert Nelson <robertcnelson@gmail.com>
2025-07-01 17:39:11 -0500

    bb-beagle-version (1.20250701.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-beagle-version/version.sh
+++ bbb.io-beagle-version/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-beagle-version"
 debian_pkg_name="${package_name}"
-package_version="1.20240930.0"
+package_version="1.20250701.0"
 package_source=""
 src_dir=""
 
@@ -19,8 +19,5 @@ debian_patch=""
 
 clear_changelog="enable"
 
-trixie_version="~trixie+20240930"
-sid_version="~sid+20240930"
-lunar_version="~lunar+20240930"
-mantic_version="~mantic+20240930"
-noble_version="~noble+20240930"
+trixie_version="~trixie+20250701"
+noble_version="~noble+20250701"
