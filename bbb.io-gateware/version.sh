36e859af7a90693aa7a9e214c5072ac3e9517f25
Robert Nelson <robertcnelson@gmail.com>
2025-07-01 17:48:34 -0500

    bbb.io-gateware (1.20250701.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-gateware/version.sh
+++ bbb.io-gateware/version.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-VERSION="BVF-0.5.1-25-g6ba5ed3"
-DATEC="1.20250101.0"
+VERSION="BVF-0.5.1-50-g00f6c5d"
+DATEC="1.20250701.0"
 
 package_name="bbb.io-gateware"
 debian_pkg_name="${package_name}"
-package_version="1.20250101.0"
+package_version="1.20250701.0"
 package_source=""
 src_dir=""
 
@@ -18,10 +18,9 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="https://openbeagle.org/beaglev-fire/gateware-snapshots/-/raw/main/${VERSION}/bbb.io-gateware_${DATEC}-0~noble_all.deb"
+debian_dl_1="http://forgejo.gfnd.rcn-ee.org:3000/BeagleBoard.org/gateware-snapshots/raw/branch/main/${VERSION}/bbb.io-gateware_${DATEC}-0~noble_all.deb"
 
 clear_changelog="enable"
 
-trixie_version="~trixie+20250108"
-sid_version="~sid+20250108"
-noble_version="~noble+20250108"
+trixie_version="~trixie+20250701"
+noble_version="~noble+20250701"
