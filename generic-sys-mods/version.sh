15f696d697e8474d2c18ed342071e7218241b9f9
Robert Nelson <robertcnelson@gmail.com>
2025-08-07 16:22:32 -0500

    generic-sys-mods (1.20250807.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- generic-sys-mods/version.sh
+++ generic-sys-mods/version.sh
@@ -2,7 +2,7 @@
 
 package_name="generic-sys-mods"
 debian_pkg_name="${package_name}"
-package_version="1.20250701.1"
+package_version="1.20250807.0"
 package_source=""
 src_dir=""
 
@@ -17,5 +17,5 @@ debian_patch=""
 
 clear_changelog="enable"
 
-trixie_version="~trixie+20250701"
-noble_version="~noble+20250701"
+trixie_version="~trixie+20250807"
+noble_version="~noble+20250807"
