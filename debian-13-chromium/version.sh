e0ac6c2968c7ccfb26667615ae1ec4bb8a39d144
Robert Nelson <robertcnelson@gmail.com>
2025-10-25 12:03:23 -0500

    switch to internal server
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-13-chromium/version.sh
+++ debian-13-chromium/version.sh
@@ -2,18 +2,18 @@
 
 #https://packages.debian.org/source/trixie/chromium
 
-mirror="http://security.debian.org/debian-security"
+mirror="http://192.168.1.10/debian"
 
 package_name="chromium"
 debian_pkg_name="${package_name}"
-package_version="141.0.7390.122"
+package_version="141.0.7390.107"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
 git_repo=""
 git_sha=""
 reprepro_dir="c/${package_name}"
-dl_path="pool/updates/main/${reprepro_dir}/"
+dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-1~deb13u1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
