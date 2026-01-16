04359d5e6d2625e1ee19efdc35c8f552eb77f1ba
Robert Nelson <robertcnelson@gmail.com>
2026-01-16 16:00:15 -0600

    bbb.io-gateware fix next verison
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-gateware/version.sh
+++ bbb.io-gateware/version.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
 VERSION="2025.10-2-g4e16f48"
-DATEC="1.20260107.0"
+DATEC="1.20260107"
 
 package_name="bbb.io-gateware"
 debian_pkg_name="${package_name}"
-package_version="1.20260116.0"
+package_version="${DATEC}.0"
 package_source=""
 src_dir=""
 
@@ -18,7 +18,7 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="http://forgejo.gfnd.rcn-ee.org:3000/BeagleBoard.org/gateware-snapshots/raw/branch/main/${VERSION}/bbb.io-gateware_${DATEC}-0~noble_all.deb"
+debian_dl_1="http://forgejo.gfnd.rcn-ee.org:3000/BeagleBoard.org/gateware-snapshots/raw/branch/main/${VERSION}/bbb.io-gateware_${DATEC}.0-0~noble_all.deb"
 
 clear_changelog="enable"
 
