23d7917c418501386deecb8e6c4fa4d810fd5fb9
Robert Nelson <robertcnelson@gmail.com>
2026-04-11 18:42:28 -0500

    riscv64: bpo
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/bpo/add_to_reprepro.sh
+++ tools/bpo/add_to_reprepro.sh
@@ -18,8 +18,7 @@ run () {
 
 if [ ! "x${dist}" = "x" ] ; then
 	if [ ! "x${suite}" = "x" ] ; then
-		repo="${base}${dist}-riscv64/"
-		deb_arch=riscv64
+		repo="${base}${dist}/"
 		run
 	else
 		echo "no suite"
