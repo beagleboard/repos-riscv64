fe166467c13e8f01550c46e4eb6b3d7b772d1562
Robert Nelson <robertcnelson@gmail.com>
2026-05-14 16:47:51 -0500

    apindex (2.2-1.20231128.2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-13-apindex/version.sh
+++ debian-13-apindex/version.sh
@@ -2,7 +2,7 @@
 
 package_name="apindex"
 debian_pkg_name="${package_name}"
-package_version="2.2-1.20231128.1"
+package_version="2.2-1.20231128.2"
 package_source=""
 src_dir=""
 
@@ -17,6 +17,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-trixie_version="~trixie+20241205"
 sid_version="~sid+20241205"
-noble_version="~noble+20241205"
+trixie_version="~trixie+20260514"
+forky_version="~forky+20260514"
+noble_version="~noble+20260514"
