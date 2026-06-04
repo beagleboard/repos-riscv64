47b5b29861c808bad6a0e831420fbe5125562768
Robert Nelson <robertcnelson@gmail.com>
2026-06-04 09:58:52 -0500

    backport: cockpit
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo13-cockpit/version.sh
+++ debian-bpo13-cockpit/version.sh
@@ -4,17 +4,15 @@ mirror="https://deb.debian.org/debian"
 
 package_name="cockpit"
 debian_pkg_name="${package_name}"
-package_version="360"
+package_version="362"
 
-git_repo=""
-git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-1~bpo13+1"
-debian_untar="${package_name}_${debian_version}.debian.tar.xz"
-debian_patch=""
 
 dist="debian-trixie-riscv64"
 suite="trixie"
 deb_arch="riscv64"
+
+sbuild_options="nocheck"
