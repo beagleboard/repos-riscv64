ed9ae6f29a6d7771726d4421a04a04b8abc5c784
Robert Nelson <robertcnelson@gmail.com>
2026-07-02 09:43:56 -0500

    add gitlab
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ external-13-gitlab/add_pkg.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+base="/var/www/html/repos/"
+outgoing="/var/www/html/farm/outgoing"
+
+run () {
+	#latest=$(apt-cache madison gitlab-runner | grep packages.gitlab.com | awk '{print $3}' | sort -V | tail -1)
+	#echo "latest: ${latest}"
+	#apt-get download gitlab-runner:${deb_arch}=${latest} || true
+	#apt-get download gitlab-runner-helper-images=${latest} || true
+
+	if [ -f ./gitlab-runner-helper-images.deb ] ; then
+		rm ./gitlab-runner-helper-images.deb
+	fi
+
+	if [ -f ./gitlab-runner_riscv64.deb ] ; then
+		rm ./gitlab-runner_riscv64.deb
+	fi
+
+	wget https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner-helper-images.deb
+	wget https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_riscv64.deb
+
+	sync
+	if [ -f ./gitlab-runner_${deb_arch}.deb ] ; then
+		if [ -f ./gitlab-runner-helper-images_all.deb ] ; then
+			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner_${deb_arch}.deb
+			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner-helper-images_all.deb
+		fi
+	fi
+}
+
+runner () {
+	repo="${base}${dist}/"
+	echo ${suite}
+	run
+}
+
+start_run () {
+	deb_arch="riscv64"
+
+	dist="debian-trixie-riscv64"
+	suite="trixie" ; runner
+}
+
+if [ ! -f /tmp/cron.lock ] ; then
+	start_run
+else
+	echo "reprepro running"
+fi
+#
