6c0e0c8d88a8ad1aeb70bb5babb654f8457a3f69
Robert Nelson <robertcnelson@gmail.com>
2026-07-02 09:45:40 -0500

    more
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- external-13-gitlab/add_pkg.sh
+++ external-13-gitlab/add_pkg.sh
@@ -10,11 +10,11 @@ run () {
 	#apt-get download gitlab-runner-helper-images=${latest} || true
 
 	if [ -f ./gitlab-runner-helper-images.deb ] ; then
-		rm ./gitlab-runner-helper-images.deb
+		rm ./gitlab-runner-helper-images.deb || true
 	fi
 
 	if [ -f ./gitlab-runner_riscv64.deb ] ; then
-		rm ./gitlab-runner_riscv64.deb
+		rm ./gitlab-runner_riscv64.deb || true
 	fi
 
 	wget https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner-helper-images.deb
@@ -22,9 +22,9 @@ run () {
 
 	sync
 	if [ -f ./gitlab-runner_${deb_arch}.deb ] ; then
-		if [ -f ./gitlab-runner-helper-images_all.deb ] ; then
+		if [ -f ./gitlab-runner-helper-images.deb ] ; then
 			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner_${deb_arch}.deb
-			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner-helper-images_all.deb
+			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner-helper-images.deb
 		fi
 	fi
 }
