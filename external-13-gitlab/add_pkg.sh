cb5f4624f94d1404399dfeb6175fadaf22265fdf
Robert Nelson <robertcnelson@gmail.com>
2026-07-02 09:54:40 -0500

    add newest riscv64
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- external-13-gitlab/add_pkg.sh
+++ external-13-gitlab/add_pkg.sh
@@ -4,27 +4,15 @@ base="/var/www/html/repos/"
 outgoing="/var/www/html/farm/outgoing"
 
 run () {
-	#latest=$(apt-cache madison gitlab-runner | grep packages.gitlab.com | awk '{print $3}' | sort -V | tail -1)
-	#echo "latest: ${latest}"
-	#apt-get download gitlab-runner:${deb_arch}=${latest} || true
-	#apt-get download gitlab-runner-helper-images=${latest} || true
-
-	if [ -f ./gitlab-runner-helper-images.deb ] ; then
-		rm ./gitlab-runner-helper-images.deb || true
-	fi
-
-	if [ -f ./gitlab-runner_riscv64.deb ] ; then
-		rm ./gitlab-runner_riscv64.deb || true
-	fi
-
-	wget https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner-helper-images.deb
-	wget https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_riscv64.deb
-
+	latest=$(apt-cache madison gitlab-runner | grep packages.gitlab.com | awk '{print $3}' | sort -V | tail -1)
+	echo "latest: ${latest}"
+	apt-get download gitlab-runner:${deb_arch}=${latest} || true
+	apt-get download gitlab-runner-helper-images=${latest} || true
 	sync
-	if [ -f ./gitlab-runner_${deb_arch}.deb ] ; then
-		if [ -f ./gitlab-runner-helper-images.deb ] ; then
-			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner_${deb_arch}.deb
-			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner-helper-images.deb
+	if [ -f ./gitlab-runner_${latest}_${deb_arch}.deb ] ; then
+		if [ -f ./gitlab-runner-helper-images_${latest}_all.deb ] ; then
+			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner_${latest}_${deb_arch}.deb
+			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner-helper-images_${latest}_all.deb
 		fi
 	fi
 }
