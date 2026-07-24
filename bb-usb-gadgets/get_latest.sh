43b15b3389a7da0e56567d0a40960ba54b68407e
Robert Nelson <robertcnelson@gmail.com>
2026-07-24 09:25:53 -0500

    bb-usb-gadgets (1.20260724.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-usb-gadgets/get_latest.sh
+++ bb-usb-gadgets/get_latest.sh
@@ -5,27 +5,16 @@ dl_src () {
 		rm -rf ./src/
 	fi
 
-	git clone https://github.com/beagleboard/customizations.git ./src/
-	#git clone git@github.com:rcn-ee/customizations.git ./src/
+	git clone https://openbeagle.org/beagleboard/usr-customizations.git ./src/
 }
 
 copy_all () {
-	cp -v ./src${wdir}${wfile} ./suite/focal/debian/
-	cp -v ./src${wdir}${wfile} ./suite/bionic/debian/
-	cp -v ./src${wdir}${wfile} ./suite/stretch/debian/
-	cp -v ./src${wdir}${wfile} ./suite/buster/debian/
-	cp -v ./src${wdir}${wfile} ./suite/bullseye/debian/
+	cp -v ./src${wdir}${wfile} ./suite/trixie/debian/
+	cp -v ./src${wdir}${wfile} ./suite/forky/debian/
+	cp -v ./src${wdir}${wfile} ./suite/noble/debian/
 }
 
 dl_src
 
-wfile="autoconfigure_usb0.sh"                 ; wdir="/usr/bin/" ; copy_all
-wfile="autoconfigure_usb1.sh"                 ; wdir="/usr/bin/" ; copy_all
-wfile="bb_dnsmasq_config.sh"                  ; wdir="/usr/bin/" ; copy_all
-wfile="bb_generate_mac.sh"                    ; wdir="/usr/bin/" ; copy_all
-wfile="bbb-acm-mass_storage-ncm-rndis.scheme" ; wdir="/etc/gt/templates/" ; copy_all
-wfile="bbb-acm-ncm-rndis.scheme"              ; wdir="/etc/gt/templates/" ; copy_all
-wfile="bbb-acm.scheme"                        ; wdir="/etc/gt/templates/" ; copy_all
-wfile="bbb-ncm-rndis.scheme"                  ; wdir="/etc/gt/templates/" ; copy_all
-
-
+wfile="bb-start-acm-ncm-rndis-old-gadget"     ; wdir="/arm64/usr/bin/" ; copy_all
+wfile="bb-start-usb-gadgets"                  ; wdir="/arm32/usr/bin/" ; copy_all
